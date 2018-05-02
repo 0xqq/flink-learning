ccc236f1b2be1750ef725ddea0d04a736394e6bc
weiyu <weiyu@gomeholdings.com>
2018-05-02 11:27:19 +0800

    添加java版本及scala版本wordcount代码

---
+++ flink-core/src/main/java/com/weiyu/bigData/flink/JavaWordCount.java
@@ -0,0 +1,59 @@
+package com.weiyu.bigData.flink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+import java.io.File;
+
+/**
+ * @author weiyu@gomefinance.com.cn
+ * @description
+ * @create 2018/5/2 10:25
+ * @since 1.0.0
+ */
+public class JavaWordCount {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<String> lines = env.readTextFile("E:" + File.separator + "test.txt");
+        DataStream<WordWithCount> windowCounts = lines.flatMap(new FlatMapFunction<String, WordWithCount>() {
+            public void flatMap(String line, Collector<WordWithCount> collector) throws Exception {
+                for (String word : line.split("\\s+")) {
+                    collector.collect(new WordWithCount(word, 1L));
+                }
+            }
+        }).keyBy("word")
+                .reduce(new ReduceFunction<WordWithCount>() {
+                    public WordWithCount reduce(WordWithCount a, WordWithCount b) throws Exception {
+                        return new WordWithCount(a.word,a.count+b.count);
+                    }
+                });
+
+        windowCounts.print().setParallelism(1);
+        env.execute("java wordCount demo");
+    }
+
+    public static class WordWithCount {
+
+        public String word;
+        public long count;
+
+        public WordWithCount() {}
+
+        public WordWithCount(String word, long count) {
+            this.word = word;
+            this.count = count;
+        }
+
+        @Override
+        public String toString() {
+            return word + " : " + count;
+        }
+    }
+}
--- flink-core/src/main/scala/com/weiyu/bigData/flink/ScalaWordCount.scala
+++ flink-core/src/main/scala/com/weiyu/bigData/flink/ScalaWordCount.scala
@@ -1,13 +1,13 @@
-package com.weiyu.bigData.flink.wordCount
+package com.weiyu.bigData.flink
 
 import java.io.File
 
-import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.api.scala._
 
-object WordCountDemo {
+object ScalaWordCount {
 
   def main(args: Array[String]): Unit = {
-    val env = ExecutionEnvironment.createLocalEnvironment(1)
+    val env = ExecutionEnvironment.getExecutionEnvironment
     val text = env.readTextFile("E:"+File.separator+"test.txt")
 
     val counts = text.flatMap { _.toLowerCase.split("\\W+") filter { _.nonEmpty } }
