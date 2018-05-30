15f2b34737676c72009a5e1920aac36306947a30
weiyu <weiyu@gomeholdings.com>
2018-05-30 18:02:48 +0800

    flink版本升级到1.5

--- src/main/scala/willem/weiyu/bigData/flink/sink/ScalaHdfsSink.scala
+++ src/main/scala/willem/weiyu/bigData/flink/sink/ScalaHdfsSink.scala
@@ -1,6 +1,7 @@
 package willem.weiyu.bigData.flink.sink
 
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.connectors.fs.StringWriter
 import org.apache.flink.streaming.connectors.fs.bucketing.{BucketingSink, DateTimeBucketer}
 
 object ScalaHdfsSink {
@@ -9,10 +10,14 @@ object ScalaHdfsSink {
 
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     val text = env.socketTextStream("10.152.18.36", 9999)
-
-    val hdfsSink = new BucketingSink[String]("hdfs://gmbdc-test/user/weiyu/test/flink")
-    hdfsSink.setBucketer(new DateTimeBucketer[String]("yyyy-mm-dd"))
-    //hdfsSink.setWriter(new SequenceFileWriter[IntWritable,Text]())
+    //修改写入hdfs用户名
+    System.setProperty("HADOOP_USER_NAME","gmjk")
+    val hdfsSink = new BucketingSink[String]("hdfs://10.152.21.12:8020/user/weiyu/test/flink")
+    hdfsSink.setBucketer(new DateTimeBucketer[String]("yyyy-MM-dd"))
+    hdfsSink.setWriter(new StringWriter[String]())
+    hdfsSink.setUseTruncate(false)
+    hdfsSink.setInactiveBucketCheckInterval(1)
+    hdfsSink.setInactiveBucketThreshold(5)
     hdfsSink.setBatchSize(1024*1024*1024)
 
     text.addSink(hdfsSink)
